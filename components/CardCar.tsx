5784cc99b06c5a96c1bac10fb4558b8a79e992b8
Trang <ngotrang2508.forwork@gmail.com>
2024-07-12 15:45:18 +0700

    suggest car

--- components/CardCar.tsx
+++ components/CardCar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import {
     StyleSheet,
     Dimensions,
@@ -10,93 +10,118 @@ import {
     FlatList,
     ListRenderItemInfo,
 } from 'react-native';
-import { useNavigation, NavigationProp } from '@react-navigation/native';
 import { TabBarIcon } from './navigation/TabBarIcon';
+import { AuthConText } from '@/store/AuthContext';
+import { useRouter } from 'expo-router';
+import axios from 'axios';
+import { apiCar } from '@/api/apiConfig';
+import { Divider } from 'react-native-paper';
 
 type Item = {
-    img: string;
-    name: string;
+    id: number;
+    car_model: {
+        id: number;
+        brand: string;
+        model: string;
+        year: string;
+        number_of_seats: number;
+    };
+    license_plate: string;
     price: number;
-    stars: number;
-    reviews: number;
-    saved: boolean;
+    images: string[];
+    rating: number;
+    total_trip: number;
 };
-
-const items: Item[] = [
-    {
-        img: 'https://images.unsplash.com/photo-1623659248894-1a0272243054?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2405&q=80',
-        name: 'Audi R8',
-        price: 158600,
-        stars: 4.45,
-        reviews: 124,
-        saved: true,
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1580274455191-1c62238fa333?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80',
-        name: 'Porsche 911',
-        price: 160100,
-        stars: 4.81,
-        reviews: 409,
-        saved: false,
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1590656364826-5f13b8e32cdc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1065&q=80',
-        name: 'Nissan GTR',
-        price: 225500,
-        stars: 4.3,
-        reviews: 72,
-        saved: false,
-    },
-];
 
 const CARD_WIDTH = Math.min(Dimensions.get('screen').width * 0.75, 400);
 
 export default function CardCar() {
-    const navigation = useNavigation<NavigationProp<any>>();
+    const authCtx = useContext(AuthConText);
+    const token = authCtx.access_token;
+    const router = useRouter();
 
-    const renderItem = ({ item }: ListRenderItemInfo<Item>) => (
-        <TouchableOpacity
-            onPress={() => {
-                // navigation.navigate('Detail');
-            }}>
-            <View style={{ paddingHorizontal: 10 }}>
-                <View style={styles.card}>
-                    <View style={styles.cardTop}>
-                        <Image
-                            alt=""
-                            resizeMode="cover"
-                            style={styles.cardImg}
-                            source={{ uri: item.img }} />
-                    </View>
-                    <View style={styles.cardBody}>
-                        <View style={styles.cardHeader}>
-                            <Text style={styles.cardTitle}>{item.name}</Text>
-                            <Text style={styles.cardPrice}>
-                                <Text style={{ fontWeight: '600' }}>{item.price.toLocaleString('en-US')} đ </Text>/
-                                ngày
-                            </Text>
+    const [cars, setCars] = useState<Item[]>([]);
+
+    useEffect(() => {
+        getSuggestCar();
+    }, []);
+
+    const getSuggestCar = async () => {
+        try {
+            const response = await axios.get(apiCar.fetchSuggestCar, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            });
+            setCars(response.data.data);
+            console.log("Fetch suggest car successfully: ", response.data.message);
+        } catch (error: any) {
+            console.log("Error fetch suggest car: ", error.response?.data?.message || error.message);
+        }
+    };
+
+    const renderItem = ({ item }: ListRenderItemInfo<Item>) => {
+        const parsedStartDate = new Date();
+        parsedStartDate.setHours(parsedStartDate.getHours() + 2);
+
+        const parsedEndDate = new Date(parsedStartDate);
+        parsedEndDate.setHours(parsedEndDate.getHours() + 22);
+
+        return (
+            <TouchableOpacity
+                onPress={() => {
+                    router.push({
+                        pathname: "/detail",
+                        params: {
+                            carId: item.id,
+                            startDate: parsedStartDate.toISOString(),
+                            endDate: parsedEndDate.toISOString(),
+                        },
+                    });
+                }}
+            >
+                <View style={styles.carItem}>
+                    <Image source={{ uri: item.images[0] }} style={styles.carImage} />
+                    <View style={styles.carDetails}>
+                        <Text style={styles.carName}>
+                            {item.car_model.brand} {item.car_model.model} {item.car_model.year}
+                        </Text>
+                        <View style={styles.carFooter}>
+                            <View style={styles.starContainer}>
+                                <TabBarIcon name="star" color="#F4CE14" size={24} style={{ marginRight: 6 }} />
+                                <Text style={styles.rating}>{item.rating}</Text>
+                            </View>
+                            <View style={styles.tripContainer}>
+                                <TabBarIcon name="history" color="green" size={24} style={{ marginRight: 6 }} />
+                                <Text style={styles.tripCount}>{item.total_trip} chuyến</Text>
+                            </View>
                         </View>
-                        <View style={styles.cardFooter}>
-                            <TabBarIcon name='star' size={24} color='#F4CE14' />
-                            <Text style={styles.cardStars}>{item.stars}</Text>
-                            <Text style={styles.cardReviews}>({item.reviews} reviews)</Text>
+                        <Divider style={{ marginTop: 15 }} />
+                        <View style={{ marginTop: 10, justifyContent: 'flex-end', alignItems: 'flex-end' }}>
+                            <Text style={styles.carPrice}>{item.price.toLocaleString('en-US')} VNĐ / ngày</Text>
                         </View>
                     </View>
                 </View>
-            </View>
-        </TouchableOpacity>
-    );
+            </TouchableOpacity>
+        );
+    };
 
     return (
         <SafeAreaView style={styles.container}>
-            <FlatList
-                data={items}
-                renderItem={renderItem}
-                keyExtractor={(item, index) => index.toString()}
-                horizontal
-                showsHorizontalScrollIndicator={false}
-                contentContainerStyle={styles.scrollViewContent}
-            />
+            {cars.length > 0 ? (
+                <FlatList
+                    data={cars}
+                    renderItem={renderItem}
+                    keyExtractor={(item) => item.id.toString()}
+                    horizontal
+                    showsHorizontalScrollIndicator={false}
+                    contentContainerStyle={styles.scrollViewContent}
+                />
+            ) : (
+                <View style={styles.emptyContainer}>
+                    <Text style={styles.emptyText}>Chưa có xe phù hợp dành cho bạn</Text>
+                </View>
+            )}
         </SafeAreaView>
     );
 }
@@ -110,12 +135,10 @@ const styles = StyleSheet.create({
     scrollViewContent: {
         paddingHorizontal: 18,
     },
-    /** Card */
-    card: {
-        position: 'relative',
+    carItem: {
         borderRadius: 8,
         backgroundColor: '#fff',
-        marginBottom: 16,
+        marginBottom: 20,
         shadowColor: '#000',
         shadowOffset: {
             width: 0,
@@ -124,66 +147,59 @@ const styles = StyleSheet.create({
         shadowOpacity: 0.2,
         shadowRadius: 1.41,
         elevation: 2,
-        width: 320
+        width: 320,
     },
-    cardLikeWrapper: {
-        position: 'absolute',
-        zIndex: 1,
-        top: 12,
-        right: 12,
-    },
-    cardLike: {
-        width: 48,
-        height: 48,
-        borderRadius: 9999,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    cardTop: {
-        borderTopLeftRadius: 8,
-        borderTopRightRadius: 8,
-    },
-    cardImg: {
+    carImage: {
         width: '100%',
         height: 160,
         borderTopLeftRadius: 8,
         borderTopRightRadius: 8,
     },
-    cardBody: {
-        padding: 12,
+    carDetails: {
+        padding: 18,
     },
-    cardHeader: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-    },
-    cardTitle: {
+    carName: {
         fontSize: 17,
-        fontWeight: '500',
+        fontWeight: 'bold',
         color: '#232425',
     },
-    cardPrice: {
+    carPrice: {
         fontSize: 15,
-        fontWeight: '400',
-        color: '#232425',
+        fontWeight: 'bold',
+        color: '#5457FB',
     },
-    cardFooter: {
-        marginTop: 8,
+    carFooter: {
         flexDirection: 'row',
         alignItems: 'center',
-        justifyContent: 'flex-start',
+        marginTop: 12,
     },
-    cardStars: {
-        marginLeft: 2,
-        marginRight: 6,
+    starContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginRight: 25,
+    },
+    rating: {
         fontSize: 14,
         fontWeight: '500',
         color: '#232425',
     },
-    cardReviews: {
+    tripContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    tripCount: {
         fontSize: 14,
-        fontWeight: '400',
-        color: '#595a63',
+        fontWeight: '500',
+        color: '#232425',
+    },
+    emptyContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    emptyText: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: '#B4B4B8',
     },
 });
